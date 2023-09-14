bb76c5217054ca278dbc2624e7f4f9ac589f8412
Astariul <remondnicola@gmail.com>
2023-09-14 19:30:17 +0900

    🔖 Set version to 0.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extras_require["dev"] = (
 
 setuptools.setup(
     name="lstm",
-    version="1.0.0.dev0",
+    version="0.1.0",
     author="Nicolas REMOND",
     author_email="remondnicola@gmail.com",
     description="Pytorch implementation of LSTM variants",
